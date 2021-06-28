9ef54a9fefdba1bffcc92e47ebfd4842f1e21f6f
Tomohiro <tomohiro@Tomohiro-Macbook-Pro.local>
2021-06-28 20:02:20 +0900

    make inference file

--- ex_9/t_suzuki/inference.py
+++ ex_9/t_suzuki/inference.py
@@ -60,7 +60,7 @@ def plot_confusion_matrix(predict, ground_truth, title=None, cmap=plt.cm.Blues):
     plt.ylabel("Predicted")
     plt.xlabel("Ground truth")
     plt.show(block=True)
-    fig.savefig(f'./out/confusion_matrix.png')
+    # fig.savefig(f'./out/confusion_matrix.png')
 
 
 
@@ -80,7 +80,7 @@ def write_result(paths, outputs):
         for path, output in zip(paths, outputs):
             f.write("{path},{output}\n".format(path=path, output=output))
 
-# definition MLP
+
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -96,7 +96,6 @@ class Net(nn.Module):
 
 
 def main():
-    # argument setting
     parser = argparse.ArgumentParser()
     parser.add_argument("--path_to_truth", type=str, help='テストデータの正解ファイルCSVのパス')
     parser.add_argument("--batch_size", type=int, default=16, help='training batch size')
@@ -104,139 +103,96 @@ def main():
     parser.add_argument("--epochs", type=int, default=100, help='number of epochs')
     args = parser.parse_args()
 
+    if not os.path.exists('./out'):
+        os.makedirs('./out')
+
     batch_size = args.batch_size
     lr = args.lr
     epochs = args.epochs
- 
-    # check output dir exist
-    if not os.path.exists('./out'):
-        os.makedirs('./out')
     
-    # data loading
     training = pd.read_csv("../training.csv")
     test = pd.read_csv("../test.csv")
 
-    # feature extraction
     X_train = feature_extraction("../" + training["path"].values)
     X_test = feature_extraction("../" + test["path"].values)
     Y_train = training["label"]
 
-    # split data
     X_train, X_valid, Y_train, Y_valid = train_test_split(
     X_train, Y_train,
     test_size=0.2,
     random_state=20200616,
     )
 
-    # standardization
     scaler = StandardScaler()
     scaler.fit(X_train)
     x_train = scaler.transform(X_train)
     x_valid = scaler.transform(X_valid)
 
-    # transform data to tensor
     x_train = torch.from_numpy(x_train).float()
     y_train = torch.from_numpy(np.array(Y_train)).long()
     x_valid = torch.from_numpy(x_valid).float()
     y_valid = torch.from_numpy(np.array(Y_valid)).long()
 
-    # make dataset
     train_dataset = TensorDataset(x_train, y_train)
     valid_dataset = TensorDataset(x_valid, y_valid)
 
-    # make dataloader
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False)
 
-    # init model
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    net = Net().to(device)
+    for i in range(5):
 
-    # set loss function and optimizer
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adadelta(net.parameters(), lr=lr, rho=0.96)
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        net = Net().to(device)
 
-    # trainig step
-    train_loss_list, train_acc_list, val_loss_list, val_acc_list = [], [], [], []
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.Adadelta(net.parameters(), lr=lr, rho=0.96)
 
-    for epoch in range(epochs):
-        train_loss, train_acc, val_loss, val_acc = 0, 0, 0, 0
+        train_loss_list, train_acc_list, val_loss_list, val_acc_list = [], [], [], []
 
-        # training
-        net.train()
-        for i, (inputs, labels) in enumerate(train_loader):
-            inputs, labels = inputs.to(device), labels.to(device)
-            optimizer.zero_grad()
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            train_loss += loss.item()
-            acc = (outputs.max(1)[1] == labels).sum()
-            train_acc += acc.item()
-            loss.backward()
-            optimizer.step()
-            avg_train_loss = train_loss / len(train_loader.dataset)
-            avg_train_acc = train_acc / len(train_loader.dataset)
+        for epoch in range(epochs):
+            train_loss, train_acc, val_loss, val_acc = 0, 0, 0, 0
 
-        # validation
-        net.eval()
-        with torch.no_grad():
-            for inputs, labels in valid_loader:
+            net.train()
+            for i, (inputs, labels) in enumerate(train_loader):
                 inputs, labels = inputs.to(device), labels.to(device)
+                optimizer.zero_grad()
                 outputs = net(inputs)
                 loss = criterion(outputs, labels)
-                val_loss += loss.item()
+                train_loss += loss.item()
                 acc = (outputs.max(1)[1] == labels).sum()
-                val_acc += acc.item()
-        avg_val_loss = val_loss / len(valid_loader.dataset)
-        avg_val_acc = val_acc / len(valid_loader.dataset)
+                train_acc += acc.item()
+                loss.backward()
+                optimizer.step()
+                avg_train_loss = train_loss / len(train_loader.dataset)
+                avg_train_acc = train_acc / len(train_loader.dataset)
 
-        print ('Epoch [{}/{}], Loss: {loss:.4f}, val_loss: {val_loss:.4f}, val_acc: {val_acc:.4f}'
-                   .format(epoch+1, epochs, i+1, loss=avg_train_loss, val_loss=avg_val_loss, val_acc=avg_val_acc))
+            net.eval()
+            with torch.no_grad():
+                for inputs, labels in valid_loader:
+                    inputs, labels = inputs.to(device), labels.to(device)
+                    outputs = net(inputs)
+                    loss = criterion(outputs, labels)
+                    val_loss += loss.item()
+                    acc = (outputs.max(1)[1] == labels).sum()
+                    val_acc += acc.item()
+            avg_val_loss = val_loss / len(valid_loader.dataset)
+            avg_val_acc = val_acc / len(valid_loader.dataset)
 
-        train_loss_list.append(avg_train_loss)
-        train_acc_list.append(avg_train_acc)
-        val_loss_list.append(avg_val_loss)
-        val_acc_list.append(avg_val_acc)
+            train_loss_list.append(avg_train_loss)
+            train_acc_list.append(avg_train_acc)
+            val_loss_list.append(avg_val_loss)
+            val_acc_list.append(avg_val_acc)
 
-    # plot loss
-    fig = plt.figure()
-    plt.plot(train_loss_list, label='training')
-    plt.plot(val_loss_list, label='validation')
-    plt.xlabel('Iterations')
-    plt.ylabel('Loss')
-    plt.legend()
-    plt.show(block=True)
-    fig.savefig(f'./out/loss_batch{batch_size}_epoch{epochs}_lr{lr}.png')
-    plt.close()
+        x_test = scaler.transform(X_test)
+        x_test = torch.from_numpy(x_test).float()
+        outputs = net(x_test)
+        pred = outputs.max(1)[1]
 
-    # plot acc
-    fig = plt.figure()
-    plt.plot(train_acc_list, label='training')
-    plt.plot(val_acc_list, label='validation')
-    plt.xlabel('Iterations')
-    plt.ylabel('Accuracy')
-    plt.legend()
-    plt.show(block=True)
-    fig.savefig(f'./out/acc_batch{batch_size}_epoch{epochs}_lr{lr}.png')
-    plt.close()
-
-    # predict test data
-    x_test = scaler.transform(X_test)
-    x_test = torch.from_numpy(x_test).float()
-    outputs = net(x_test)
-    pred = outputs.max(1)[1]
-
-    # evaluate test output
-    if args.path_to_truth:
-        test_truth = pd.read_csv(args.path_to_truth)
-        truth_values = test_truth['label'].values
-        test_acc = accuracy_score(truth_values, pred)
-        plot_confusion_matrix(pred, truth_values, f'Accuracy: {test_acc:.2%}')
-        print("Test accuracy: ", test_acc)
-
-    # saev model
-    model_path = f'./model/model_acc_{test_acc:.2%}.pth'
-    torch.save(net.to(device).state_dict(), model_path)
+        if args.path_to_truth:
+            test_truth = pd.read_csv(args.path_to_truth)
+            truth_values = test_truth['label'].values
+            test_acc = accuracy_score(truth_values, pred)
+            print("Test accuracy: ", test_acc)
 
 
 if __name__ == "__main__":
